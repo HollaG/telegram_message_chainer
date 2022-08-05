12de8c8fbbf26473368d383160711e5e1b899ecb
Marcus Soh <marcussoh38@gmail.com>
2022-08-05 23:12:08 +0800

    progress on adding inline mode

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { Context, NarrowedContext, Telegraf } from "telegraf";
 import { Update } from "typegram";
 import { Chain } from "./ChainClass";
 import * as fs from "fs";
-import { Message } from "telegraf/typings/core/types/typegram";
+import {
+    InlineQueryResult,
+    InlineQueryResultArticle,
+    Message,
+} from "telegraf/typings/core/types/typegram";
 require("dotenv").config();
 
 export let defaultMsg = `Reply to this message to continue the chain!\nA second reply will overwrite your first \n\nChains will end automatically after 1 week`;
@@ -18,8 +22,46 @@ export type Data = {
 };
 let data: Data = {};
 
+export type InlineChainData = {
+    [creatorId: number]: Chain[];
+};
+let inlineChainData: InlineChainData = {};
+
+let inlineCreationData: {
+    [chatId: number]: 1;
+} = {};
+
+let inlineReplyData: {
+    [chatId: number]: string; // `${creatorId}_-_${chainTitle}`
+} = {};
+
 // Todo figure out the typescript typings for ctx and extract duplicated code into a function
 bot.start(async (ctx) => {
+    console.log(ctx.startPayload);
+    if (ctx.startPayload === "inline") {
+        ctx.reply("Let's create a new chain. Please send me your chain title.");
+        inlineCreationData[ctx.chat.id] = 1;
+        return;
+    }
+
+    if (ctx.startPayload.startsWith("add")) {
+        // prompt user to enter their chain message
+        const chainCreatorId = Number(ctx.startPayload.split("_-_")[1]);
+        const chainTitle = ctx.startPayload.split("_-_")[2];
+
+        // find the chain
+        const chain = inlineChainData[chainCreatorId].find(
+            (chain) => chain.title === chainTitle
+        );
+
+        if (!chain) return ctx.reply("Chain not found");
+
+        // prompt user to enter their chain message
+        ctx.reply(`Please enter your message for the chain "${chain.title}".`);
+
+        inlineReplyData[ctx.chat.id] = `${chainCreatorId}_-_${chainTitle}`;
+    }
+
     if (ctx.chat.type === "private")
         return ctx.reply(`Sorry, this bot only works in groups!`);
 
@@ -79,9 +121,120 @@ bot.command("chain", async (ctx) => {
     backupAndClear(ctx);
 });
 
-bot.on("text", (ctx) => {
+bot.on("text", async (ctx) => {
     // ignore private msgs
-    if (ctx.chat.type === "private") return;
+    if (ctx.chat.type === "private") {
+        // Check if user is entering a chain title for an inline chain
+        if (inlineCreationData[ctx.chat.id]) {
+            // check conditions
+            // 1) Not empty
+            // 2) Not longer than 256 characters
+            const sanitizedTitle = ctx.message.text.trim();
+            if (sanitizedTitle.length > 256) {
+                ctx.reply("Chain title must be less than 256 characters");
+                return;
+            }
+            if (sanitizedTitle.length === 0) {
+                ctx.reply("Chain title cannot be empty");
+                return;
+            }
+
+            // Chain title successfully received
+            // Create new chain and send it to the user
+            let msgText = `<b><u> ${sanitizedTitle} </u></b>\n\n<i>No respondents yet!</i>`;
+            const msg = await ctx.reply(`Please wait...`);
+
+            const msgId = msg.message_id;
+
+            const uniqueChainId = `${ctx.chat.id}:${msgId}`;
+            await ctx.telegram.editMessageText(
+                ctx.chat.id,
+                msgId,
+                undefined,
+                msgText,
+                {
+                    parse_mode: "HTML",
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                {
+                                    switch_inline_query: sanitizedTitle,
+                                    text: "Share chain",
+                                },
+                                {
+                                    text: "End chain",
+                                    callback_data: "end",
+                                },
+                            ],
+                        ],
+                    },
+                }
+            );
+
+            delete inlineCreationData[ctx.chat.id];
+
+            // save the chain
+            if (!inlineChainData[ctx.chat.id])
+                inlineChainData[ctx.chat.id] = [];
+            inlineChainData[ctx.chat.id].push(
+                new Chain(ctx.from.first_name, sanitizedTitle)
+            );
+
+            backupAndClearInlineChains(ctx);
+        }
+
+        if (inlineReplyData[ctx.chat.id]) {
+            // check conditions
+            // 1) Not empty
+            // 2) Not longer than 256 characters
+            const sanitizedMsg = ctx.message.text.trim();
+            if (sanitizedMsg.length > 256) {
+                ctx.reply("Message must be less than 256 characters");
+                return;
+            }
+            if (sanitizedMsg.length === 0) {
+                ctx.reply("Message cannot be empty");
+                return;
+            }
+
+            // Message successfully received
+            // Create new chain and send it to the user
+            const chainCreatorId = Number(
+                inlineReplyData[ctx.chat.id].split("_-_")[0]
+            );
+            const chainTitle = inlineReplyData[ctx.chat.id].split("_-_")[1];
+
+            // find the chain
+            const chain = inlineChainData[chainCreatorId].find(
+                (chain) => chain.title === chainTitle
+            );
+
+            if (!chain) return ctx.reply("Chain not found");
+
+            // add the message to the chain
+            chain.updateReplies({
+                first_name: ctx.from.first_name,
+
+                memberId: ctx.from.id,
+                text: sanitizedMsg,
+                username: ctx.from.username || "",
+            });
+
+            // edit the just sent message to add the ID and stuff at the bottom
+            // ctx.telegram.editMessageText(
+            //     ctx.chat.id,
+            //     chain.lastMessageId,
+            //     undefined,
+            //     chain.generateReplyMessage(ctx.chat.id, chain.lastMessageId),
+            //     {
+            //         parse_mode: "HTML",
+            //     }
+            // );
+
+            // delete the inline data
+            delete inlineReplyData[ctx.chat.id];
+        }
+    }
 
     // ignore messages not a reply
     if (!ctx.message.reply_to_message) return;
@@ -113,6 +266,105 @@ bot.on("text", (ctx) => {
 
     backupAndClear(ctx);
 });
+
+/* Inline Queries */
+bot.on("inline_query", async (ctx) => {
+    console.log("Hello from inline query");
+    const query = ctx.inlineQuery.query.trim();
+    console.log(ctx.inlineQuery);
+    // Look for the chain belonging to this user with the specified search terms only if it exists
+    console.log(inlineChainData[ctx.from.id]);
+    if (inlineChainData[ctx.from.id]) {
+        const chains = inlineChainData[ctx.from.id].filter((chain) => {
+            // match the chain title
+            const regex = `/.*${query}.*/i`;
+            console.log({ regex });
+            if (chain.title.includes(query)) return true;
+        });
+
+        console.log({ chains });
+        if (chains.length) {
+            const inlineAnswers: InlineQueryResultArticle[] = chains.map(
+                (chain) => ({
+                    type: "article",
+                    id: "2",
+
+                    thumb_url: "https://i.imgur.com/FYEGVYV.jpg",
+
+                    title: chain.title,
+                    input_message_content: {
+                        message_text: chain.title,
+                    },
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                {
+                                    text: "Add your message",
+                                    url: `https://t.me/msgchainbot?start=add_-_${ctx.from.id}_-_${chain.title}`,
+                                },
+                            ],
+                            [
+                                {
+                                    text: "Activate chain",
+                                    callback_data: "activate",
+                                },
+                            ],
+                        ],
+                    },
+                })
+            );
+            const msg = await ctx.answerInlineQuery(
+                inlineAnswers,
+
+                {
+                    switch_pm_parameter: "inline",
+                    switch_pm_text: "Create a new chain",
+                    cache_time: 0,
+                }
+            );
+        } else {
+            ctx.answerInlineQuery(
+                [
+                    {
+                        type: "photo",
+                        id: "1",
+                        photo_url: "https://i.imgur.com/FYEGVYV.jpg",
+                        thumb_url: "https://i.imgur.com/FYEGVYV.jpg",
+                    },
+                ],
+
+                {
+                    switch_pm_parameter: "inline",
+                    switch_pm_text: "Create a new chain",
+                    cache_time: 0,
+
+                }
+            );
+        }
+    } else {
+        ctx.answerInlineQuery(
+            [
+                {
+                    type: "photo",
+                    id: "1",
+                    photo_url: "https://i.imgur.com/FYEGVYV.jpg",
+                    thumb_url: "https://i.imgur.com/FYEGVYV.jpg",
+                },
+            ],
+
+            {
+                switch_pm_parameter: "inline",
+                switch_pm_text: "Create a new chain",
+                cache_time: 0,
+
+            }
+        );
+    }
+});
+
+// bot.on('callback_query', async (ctx) => {
+//     ctx.telegram.editMessageText(ctx.chat?.id, )
+// })
 
 // handle loading from backup file
 const backupAndClear = async (ctx: Context) => {
@@ -146,6 +398,17 @@ const backupAndClear = async (ctx: Context) => {
     fs.writeFile("data.json", JSON.stringify(data), (e) => {
         if (e) return console.log(e);
     });
+};
+
+const backupAndClearInlineChains = async (ctx: Context) => {
+    // todo : clear
+    fs.writeFile(
+        "inlineChainData.json",
+        JSON.stringify(inlineChainData),
+        (e) => {
+            if (e) return console.log(e);
+        }
+    );
 };
 
 // reload objects into memory if crash
